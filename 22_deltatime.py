3cf8cc93e4579d9a37cc60df2531c171f8cba50c
liupy525 <liupy525@gmail.com>
2014-01-29 11:50:12 +0800

    change file's names

--- 22_deltatime.py
+++
@@ -1,9 +0,0 @@
-import datetime,time
-def dateDiffInSeconds(date1, date2):
-    timedelta = date2 - date1
-    return timedelta.days*24*3600 + timedelta.seconds
-
-date1 = datetime.datetime(2006,02,17,15,30,00)
-date2 = datetime.datetime(2006,05,18,11,01,00)
-
-print dateDiffInSeconds(date1, date2)
--- 33_dashu.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-a = 99999
-n = 99999999999
-
-print pow(a,n,20132013)
